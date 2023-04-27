b51b08cedd7683f4d35f1923f3db88f69617329b
Jaime A <jaimeangel1097@gmail.com>
2023-04-27 15:53:02 -0500

    agragando formulario para obtener noticias

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import {Container,Typography,Grid} from '@mui/material';
+import Formulario from './components/Formulario';
+
 function App() {
   return (
-    <>
-      <h1>APP HERE</h1>
-    </>
+    <Container>
+
+      <header>
+        <Typography align='center' variant='h3' component='h1' marginY={5}>
+            ESTO ES UN TITULO
+        </Typography>
+      </header>
+      <Grid 
+        container
+        direction='row'
+        justifyContent='center'
+      >
+        <Grid item md={7} xs={12} lg={7}>
+          <Formulario/>
+        </Grid>
+      </Grid>
+
+    </Container>
   )
 }
 
