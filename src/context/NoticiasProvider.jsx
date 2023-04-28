9cfa93048b9ebccf49964f5eee6b06e7fef0c00d
Jaime A <jaimeangel1097@gmail.com>
2023-04-28 10:09:16 -0500

    consultando API de noticias y guardando resultado en state

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,15 +1,26 @@
-import { createContext , useState} from "react"
+import axios from 'axios'
+import {createContext,useState,useEffect} from "react"
 
 const NoticiasContext=createContext()
 
 function NoticiasProvider({children}) {
 
   const [categoria,setCategoria]=useState('general')
+  const [noticias,setNoticias]=useState([])
 
   const handleSetCategoria=(e)=>{
     e.preventDefault()
     setCategoria(e.target.value)
   }
+
+  useEffect(()=>{
+    const getNoticias=async ()=>{
+      const url=`https://newsapi.org/v2/top-headlines?category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+      const {data:{articles}}= await axios(url)
+      setNoticias(articles)
+    }
+    getNoticias()
+  },[categoria])
 
   return (
     <NoticiasContext.Provider
