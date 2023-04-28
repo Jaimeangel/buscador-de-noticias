5113da233762da7c6ac1ad9a956b289c42176aa7
Jaime A <jaimeangel1097@gmail.com>
2023-04-28 17:52:51 -0500

    creando paginacion

--- src/components/ListadoNoticias.jsx
+++ src/components/ListadoNoticias.jsx
@@ -1,35 +1,51 @@
-import {Typography,Grid} from '@mui/material';
+import {Typography,Grid,Stack,Pagination} from '@mui/material';
 import useNoticiasContext from '../hooks/useNoticiasContext';
 import Noticia from './Noticia';
 
 function ListadoNoticias() {
 
-    const {noticias}=useNoticiasContext()
-
+    const {noticias,totalResults,page,handleSetPage}=useNoticiasContext()
+    const totalPaginas=Math.ceil(totalResults/20) 
     return (
         <>
-        <Typography
-            textAlign={'center'}
-            variant='h4'
-            component={'h4'}
-            marginY={4}
-        >
-            Ultimas noticias
-        </Typography>
-        <Grid 
-            container
-            justifyContent={'center'}
-            spacing={3}
-        >
-            {
-                noticias.map(noticia=>(
-                    <Noticia
-                        key={noticia.url}
-                        noticia={noticia}
-                    />
-                ))
-            }
-        </Grid>
+            <Typography
+                textAlign={'center'}
+                variant='h4'
+                component={'h4'}
+                marginY={4}
+            >
+                Ultimas noticias
+            </Typography>
+            <Grid 
+                container
+                justifyContent={'center'}
+                spacing={3}
+            >
+                {
+                    noticias.map(noticia=>(
+                        <Noticia
+                            key={noticia.url}
+                            noticia={noticia}
+                        />
+                    ))
+                }
+            </Grid>
+            <Stack
+                direction={'row'}
+                justifyContent={'center'}
+                alignItems={'center'}
+                sx={{
+                    marginTop:7,
+                    marginBottom:7
+                }}
+            >
+                <Pagination 
+                    count={totalPaginas} 
+                    color="primary" 
+                    onChange={handleSetPage}
+                    page={page}
+                />
+            </Stack>
         </>
     )
 }
--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -7,27 +7,48 @@ function NoticiasProvider({children}) {
 
   const [categoria,setCategoria]=useState('general')
   const [noticias,setNoticias]=useState([])
+  const [page,setPage]=useState(1)
+  const [totalResults,setTotalResults]=useState(0)
 
   const handleSetCategoria=(e)=>{
     e.preventDefault()
     setCategoria(e.target.value)
   }
 
+  const handleSetPage=(e,value)=>{
+    setPage(value)
+  }
+
   useEffect(()=>{
     const getNoticias=async ()=>{
       const url=`https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
-      const {data:{articles}}= await axios(url)
-      setNoticias(articles)
+      const {data}= await axios(url)
+      setNoticias(data.articles)
+      setTotalResults(data.totalResults)
+      setPage(1)
     }
     getNoticias()
   },[categoria])
+
+  useEffect(()=>{
+    const getNoticias= async ()=>{
+      const url=`https://newsapi.org/v2/top-headlines?country=us&page=${page}&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+      const {data}= await axios(url)
+      setNoticias(data.articles)
+      setTotalResults(data.totalResults)
+    }
+    getNoticias()
+  },[page])
 
   return (
     <NoticiasContext.Provider
       value={{
         categoria,
         handleSetCategoria,
-        noticias
+        noticias,
+        totalResults,
+        page,
+        handleSetPage
       }}
     >
         {children}
